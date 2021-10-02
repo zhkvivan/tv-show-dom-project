fbf19641c5a87ac64b90f839b25629d23b216b68
Ivan Zhukov <zhkvivan@gmail.com>
2021-10-02 01:45:15 +0100

    sticky sidebar js file added

---
+++ sticky-sidebar.js
@@ -0,0 +1,48 @@
+// https://github.com/Krzysztof-Antosik/Two-direction-Sticky-Sidebar
+
+//aside selector
+const aside = document.querySelector('[data-sticky="true"]'),
+	//varibles
+	startScroll = 0;
+var endScroll = window.innerHeight - aside.offsetHeight - 500,
+	currPos = window.scrollY,
+	screenHeight = window.innerHeight,
+	asideHeight = aside.offsetHeight;
+aside.style.top = startScroll + 'px';
+//check height screen and aside on resize
+window.addEventListener('resize', () => {
+	screenHeight = window.innerHeight;
+	asideHeight = aside.offsetHeight;
+});
+//main function
+document.addEventListener(
+	'scroll',
+	() => {
+		endScroll = window.innerHeight - aside.offsetHeight;
+		let asideTop = parseInt(aside.style.top.replace('px;', ''));
+		if (asideHeight > screenHeight) {
+			if (window.scrollY < currPos) {
+				//scroll up
+				if (asideTop < startScroll) {
+					aside.style.top = asideTop + currPos - window.scrollY + 'px';
+				} else if (asideTop >= startScroll && asideTop != startScroll) {
+					aside.style.top = startScroll + 20 +  'px';
+				}
+			} else {
+				//scroll down
+				if (asideTop > endScroll) {
+					aside.style.top = asideTop + currPos - window.scrollY + 'px';
+				} else if (asideTop < endScroll && asideTop != endScroll) {
+					aside.style.top = endScroll + 'px';
+				}
+			}
+		} else {
+			aside.style.top = startScroll + 'px';
+		}
+		currPos = window.scrollY;
+	},
+	{
+		capture: true,
+		passive: true,
+	}
+);
